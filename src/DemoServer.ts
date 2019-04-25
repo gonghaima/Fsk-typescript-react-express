44d364ff2dcd51bebc1a215f507eba2f21b584d2
Steven <gonghaima@hotmail.com>
2019-04-25 14:27:43 +1000

    Building for Production

--- src/DemoServer.ts
+++ src/DemoServer.ts
@@ -1,8 +1,16 @@
+/**
+ * Express Server file.
+ *
+ * created by Sean Maxwell Jan 21, 2019
+ */
+
+import * as path from "path";
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as controllers from "./controllers";
+
 import { Server } from "@overnightjs/core";
 import { cimp, cinfo } from "simple-color-print";
-import DemoController from "./controllers/demo/DemoController";
 
 class DemoServer extends Server {
   private readonly _SERVER_START_MSG = "Demo server started on port: ";
@@ -16,19 +24,53 @@ class DemoServer extends Server {
   constructor() {
     super();
 
-    // Setup json middleware
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
-
-    // Setup the controllers
-    super.addControllers(new DemoController());
+    this.setupControllers();
 
     // Point to front-end code
     if (process.env.NODE_ENV !== "production") {
-      cinfo("Starting server in development mode");
-      const msg = this._DEV_MSG + process.env.EXPRESS_PORT;
-      this.app.get("*", (req, res) => res.send(msg));
+      this._serveFrontEndDev();
+    } else {
+      this._serveFrontEndProd();
     }
+  }
+
+  private setupControllers(): void {
+    const ctlrInstances = [];
+
+    for (const name in controllers) {
+      if (controllers.hasOwnProperty(name)) {
+        let Controller = (controllers as any)[name];
+        ctlrInstances.push(new Controller());
+      }
+    }
+
+    super.addControllers(ctlrInstances);
+  }
+
+  private _serveFrontEndDev(): void {
+    cinfo("Starting server in development mode");
+
+    const msg = this._DEV_MSG + this._port;
+    this.app.get("*", (req, res) => res.send(msg));
+  }
+
+  private _serveFrontEndProd(): void {
+    cinfo("Starting server in production mode");
+
+    this._port = 3002;
+
+    const dir = path.join(__dirname, "public/react/demo-react/");
+
+    // Set the static and views directory
+    this.app.set("views", dir);
+    this.app.use(express.static(dir));
+
+    // Serve front-end content
+    this.app.get("*", (req, res) => {
+      res.sendFile("index.html", { root: dir });
+    });
   }
 
   public start(): void {
